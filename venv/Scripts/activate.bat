dfcae49b652cd94cbb9af28e7eaaf46bb26af672
night <night@mail.com>
2022-08-08 23:04:38 +0500

    working ci test pipeline

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\nightblure\Desktop\dev\flask-site-ci-cd\venv
+set VIRTUAL_ENV=C:\Users\nightblure\Desktop\dev\flask-site-with-CI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
