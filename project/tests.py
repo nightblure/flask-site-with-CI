dea95c30cbbdaa0679f32304be1615f94ba37870
night <night@mail.com>
2022-08-08 21:33:48 +0500

    ci cd pipeline tests

---
+++ project/tests.py
@@ -0,0 +1,7 @@
+from functions import get_data
+
+
+def test_data():
+    data = [data_item['name'] for data_item in get_data()]
+    check_list = ['Davis Pittman', 'Davis Pittman', 'Hasad Goff', 'Wendy Hallds', 'Elaine Mueller']
+    assert check_list.sort() == data.sort()
