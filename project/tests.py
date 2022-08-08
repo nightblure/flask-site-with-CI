d5da3b51babd228862c739f4c1249aba921bd8be
night <night@mail.com>
2022-08-08 22:19:14 +0500

    make a tests bug

--- project/tests.py
+++ project/tests.py
@@ -4,4 +4,4 @@ from functions import get_data
 def test_data():
     data = [data_item['name'] for data_item in get_data()]
     check_list = ['Davis Pittman', 'Davis Pittman', 'Hasad Goff', 'Wendy Hallds', 'Elaine Mueller']
-    assert check_list.sort() == data.sort()
+    assert check_list.sort() != data.sort()
